84edb2d4c6ab37abdce315064a5bbbe8dcbdb951
HectorRamirez <eduardo.846@gmail.com>
2022-10-13 18:11:01 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills/>
+    </div>
+  );
+}
+
+export default App;
