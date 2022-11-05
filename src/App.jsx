309d16dbfa08ecc9d98ace5902c42e29c299e123
HectorRamirez <eduardo.846@gmail.com>
2022-11-05 12:29:30 -0400

    05112022-1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Banner from "./components/Banner";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Banner />
       <Skills/>
       <Projects/>
+      <Footer/>
      </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -40,8 +40,8 @@ export const NavBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink==='home' ? 'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skill" className={activeLink==='skill' ? 'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink('skill')}>Skill</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink==='projects' ? 'active navbar-link':'navbar-link'}onClick={()=>onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink==='skill' ? 'active navbar-link':'navbar-link'} onClick={()=>onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#project" className={activeLink==='projects' ? 'active navbar-link':'navbar-link'}onClick={()=>onUpdateActiveLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div  className="social-icon">
